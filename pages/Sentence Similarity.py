20d4a3f283b00556967903f4d36107f0aee397a4
sudoVenomSnake <swastikm26@gmail.com>
2024-05-27 00:18:50 +0530

    quick fix again

--- pages/Sentence Similarity.py
+++ pages/Sentence Similarity.py
@@ -37,15 +37,15 @@ query = st.text_input("Please put in your input query -")
 
 if query:
     for n_, i in enumerate(sentence_search_mutation(query)):
+        st.subheader(f"""[{i["fields"]["Case Title"][0]}]({i["fields"]["Judgement PDF URL"][0]})""")
+        st.text(i["fields"]["Case Number"][0])
         try:
-            st.subheader(f"""[{i["fields"]["Case Title"][0]}]({i["fields"]["Judgement PDF URL"][0]})""")
-            st.text(i["fields"]["Case Number"][0])
             st.markdown(" ".join(["**" + i.strip() + "**" if n == 2 else i for n, i in enumerate(i["fields"]["Sentences"])]))
-            st.text(i["fields"]["Judgement Date"][0])
-            if st.button("💬 Chat With Judgement", key = i["fields"]["Case Title"][0] + f"_{n_}"):
-                if "messages" in st.session_state:
-                    del st.session_state.messages
-                st.session_state.case_selected = i["fields"]["Case Title"][0]
-                st.switch_page("pages/Case Chat.py")
         except:
             continue
+        st.text(i["fields"]["Judgement Date"][0])
+        if st.button("💬 Chat With Judgement", key = i["fields"]["Case Title"][0] + f"_{n_}"):
+            if "messages" in st.session_state:
+                del st.session_state.messages
+            st.session_state.case_selected = i["fields"]["Case Title"][0]
+            st.switch_page("pages/Case Chat.py")
